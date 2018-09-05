add88e1c013ac9b751071315f856a704451d14d1
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-09-05 15:56:50 +0200

    Fix webpack dev server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
-const os = require('os');
+const MetaAuth = require('meta-auth');
 
 const app = express();
+const metaAuth = new MetaAuth();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+app.get('/auth/:MetaAddress', metaAuth, (req, res) => {
+  if (req.metaAuth && req.metaAuth.challenge) {
+    res.send(req.metaAuth.challenge)
+  }
+});
+
+app.get('/auth/:MetaMessage/:MetaSignature', metaAuth, (req, res) => {
+  if (req.metaAuth && req.metaAuth.recovered) {
+    res.send(req.metaAuth.recovered);
+  } else {
+    res.status(400).send();
+  };
+});
+
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,13 @@ module.exports = {
     port: 3000,
     open: true,
     proxy: {
-      '/api': 'http://localhost:8080'
+      '/api/*': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
     }
   },
   plugins: [
