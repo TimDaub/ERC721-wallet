8fc2a0a1a8cbd25b0c90616d02d741b84ffa2d21
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-10-21 21:44:41 +0200

    Test fetch transactions

--- __tests__/sagas/transactions.js
+++ __tests__/sagas/transactions.js
@@ -21,8 +21,8 @@ describe("transaction fetching", () => {
           return {
             getPastEvents: jest
               .fn()
-              .mockReturnValueOnce([{ returnValues: { _tokenId: 1 } }])
-              .mockReturnValueOnce([{ returnValues: { _tokenId: 2 } }]),
+              .mockReturnValueOnce([{ returnValues: { _tokenId: 1 } }]) //outputs
+              .mockReturnValueOnce([{ returnValues: { _tokenId: 2 } }]), // inputs
             methods: {
               tokenURI: jest.fn(() => jest.fn(() => "https://example.com")),
               name: jest.fn(() => jest.fn(() => "name"))
@@ -48,6 +48,60 @@ describe("transaction fetching", () => {
                 contract: contractAddress
               }
             ]
+          }
+        }
+      })
+      .run();
+  });
+  it("should process transactions correctly and return no token", async () => {
+    const contractAddress = "0x9326f84fcca8a136da3a4f71bbffbde6635c58da";
+    const address = "0x51Ff1fab76079d20418d1c74DA65653FfE3fD0aa";
+    localStorage.setItem("tokens", contractAddress);
+
+    config.web3 = {
+      eth: {
+        Contract: jest.fn(() => {
+          return {
+            getPastEvents: jest
+              .fn()
+              .mockReturnValueOnce([{ returnValues: { _tokenId: 1 } }]) //outputs
+              .mockReturnValueOnce([{ returnValues: { _tokenId: 1 } }]), // inputs
+            methods: {
+              tokenURI: jest.fn(() => jest.fn(() => "https://example.com")),
+              name: jest.fn(() => jest.fn(() => "name"))
+            }
+          };
+        })
+      }
+    };
+
+    const contract = new config.web3.eth.Contract(ERC721, contractAddress);
+    return expectSaga(fetchTransactions, {
+      payload: { address }
+    })
+      .put({
+        type: "FETCH_TRANSACTIONS_SUCCESS",
+        payload: {
+          transactions: {
+            [contractAddress]: []
+          }
+        }
+      })
+      .run();
+  });
+  it("shouldn't process transactions but return an empty array", async () => {
+    const contractAddress = "0x9326f84fcca8a136da3a4f71bbffbde6635c58da";
+    const address = "0x51Ff1fab76079d20418d1c74DA65653FfE3fD0aa";
+
+    const contract = new config.web3.eth.Contract(ERC721, contractAddress);
+    return expectSaga(fetchTransactions, {
+      payload: { address }
+    })
+      .put({
+        type: "FETCH_TRANSACTIONS_SUCCESS",
+        payload: {
+          transactions: {
+            [contractAddress]: []
           }
         }
       })
