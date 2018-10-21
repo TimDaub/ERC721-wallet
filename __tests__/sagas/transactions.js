7f7485a52b7967d92c1630e68f8ea4599990adc5
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-10-21 21:40:04 +0200

    Minor corrections

--- __tests__/sagas/transactions.js
+++ __tests__/sagas/transactions.js
@@ -1,25 +1,20 @@
 // @format
 import { expectSaga } from "redux-saga-test-plan";
 import { call } from "redux-saga/effects";
-import * as matchers from "redux-saga-test-plan/matchers";
-import Utils from "web3-utils";
-import FakeProvider from "web3-fake-provider";
 import Web3 from "web3";
 
 const fetchTransactions = require("../../src/sagas/transactions").__get__(
   "fetchTransactions"
 );
 import ERC721 from "../../src/abis/ERC721.json";
-import getWeb3 from "../../src/utils/getWeb3";
 import config from "../../src/config";
 
-describe("that transactions", () => {
-  it("processe correctly", async () => {
+describe("transaction fetching", () => {
+  it("should process transactions correctly and return a token", async () => {
     const contractAddress = "0x9326f84fcca8a136da3a4f71bbffbde6635c58da";
     const address = "0x51Ff1fab76079d20418d1c74DA65653FfE3fD0aa";
     localStorage.setItem("tokens", contractAddress);
 
-    const provider = new FakeProvider();
     config.web3 = {
       eth: {
         Contract: jest.fn(() => {
