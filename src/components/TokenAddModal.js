cad9cf9e39034c942c53bb3e485580fe59551885
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-10-26 13:45:00 +0200

    Add address validation for token adder

--- src/components/TokenAddModal.js
+++ src/components/TokenAddModal.js
@@ -7,21 +7,36 @@ import { toast } from "react-toastify";
 import StyledButton from "./StyledButton";
 import { addTokenBegin } from "../actions/addToken";
 import { addNameAndSymbolBegin } from "../actions/addNameAndSymbol";
+import config from "../config";
 
 const StyledInput = styled.input`
   display: block;
   font-size: 1.1em;
   width: 99%;
   height: 2em;
+  border: 1px solid grey;
+  &:focus {
+    outline: none;
+    border: 1px solid black;
+  }
 `;
 
 const StyledParagraph = styled.p`
   margin-bottom: 5px;
 `;
 
+const StyledSpan = styled.span`
+  color: red;
+  font-size: 0.5em;
+`;
+
 class TokenAddModal extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      validAddress: true
+    };
 
     this.addToken = this.addToken.bind(this);
     this.onChange = this.onChange.bind(this);
@@ -33,7 +48,14 @@ class TokenAddModal extends Component {
 
   onChange() {
     const contract = this.refs.contract.value;
-    this.props.addNameAndSymbol(contract);
+
+    const web3 = config.web3;
+    const validAddress = web3.utils.isAddress(contract);
+    this.setState({ validAddress });
+
+    if (validAddress) {
+      this.props.addNameAndSymbol(contract);
+    }
   }
 
   componentDidUpdate() {
@@ -45,17 +67,19 @@ class TokenAddModal extends Component {
 
   render() {
     const { name, symbol } = this.props;
+    const { validAddress } = this.state;
     return (
       <div>
         <h1>Add Tokens</h1>
         <StyledParagraph>Token</StyledParagraph>
         <StyledInput onChange={this.onChange} type="text" ref="contract" />
+        {validAddress ? null : <StyledSpan>Invalid Address</StyledSpan>}
         <StyledParagraph>Name</StyledParagraph>
         <StyledInput type="text" value={name} readOnly={name} />
         <StyledParagraph>Symbol</StyledParagraph>
         <StyledInput type="text" value={symbol} readOnly={symbol} />
-        <StyledButton float="right" margin="1em 0 0 0" onClick={this.addToken}>
-          Add Token
+        <StyledButton float="right" margin="2em 0 0 0" onClick={this.addToken}>
+          Next
         </StyledButton>
       </div>
     );
