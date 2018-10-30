75d856466c03f1f9edf6ca4aa582ab16665d66ff
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-10-30 11:15:22 +0100

    Ledger integration

--- src/components/Start.js
+++ src/components/Start.js
@@ -5,6 +5,7 @@ import Modal from "react-modal";
 import { toast } from "react-toastify";
 import { withRouter } from "react-router-dom";
 import { FoldingCube } from "styled-spinkit";
+import LedgerWalletSubproviderFactory from "ledger-wallet-provider";
 
 import getWeb3 from "../utils/getWeb3";
 import Headline from "./Headline";
@@ -21,19 +22,21 @@ const StyledTagLine = styled.h1`
   padding-right: 50vh;
 `;
 
-const MetamaskButton = styled.button`
+const ConnectButton = styled.button`
+  display: inline-block;
   font-family: "Ubuntu", sans-serif;
-  background-color: rgb(246, 133, 27);
+  background-color: ${props => props.color};
+  border: 1px solid ${props => props.color};
   color: white;
-  border: none;
   border-radius: 1px;
   padding: 0.5em;
   font-size: 1.3em;
+  margin: 0.75em;
   &:focus {
     outline: 0;
   }
   &:hover {
-    border 1px solid rgb(246, 133, 27);
+    border: 1px solid ${props => props.color};
     background-color: white;
     color: black;
     cursor: pointer;
@@ -60,6 +63,7 @@ class Start extends Component {
     };
     this.checkMetamaskSetup = this.checkMetamaskSetup.bind(this);
     this.checkAccountsLocked = this.checkAccountsLocked.bind(this);
+    this.checkLedgerSetup = this.checkLedgerSetup.bind(this);
   }
 
   async checkAccountsLocked() {
@@ -81,12 +85,33 @@ class Start extends Component {
     if (web3.currentProvider.isMetaMask) {
       const accounts = await web3.eth.getAccounts();
       if (accounts.length) {
-        this.props.history.push("wallet");
+        this.props.history.push({
+          pathname: "wallet",
+          search: "?provider=metamask"
+        });
       } else {
         toast.warning("Unlock Metamask");
       }
     } else {
       toast.warning("Install Metamask");
+    }
+  }
+
+  async checkLedgerSetup() {
+    if (location.protocol === "http:") {
+      toast.warning("Switch to https for Ledger to work");
+    } else {
+      const ledgerWalletSubProvider = await LedgerWalletSubproviderFactory();
+      if (ledgerWalletSubProvider.isSupported) {
+        this.props.history.push({
+          pathname: "wallet",
+          search: "?provider=ledger"
+        });
+      } else {
+        toast.warning(
+          "U2F is not supported, download plugin for Firefox or use Chrome"
+        );
+      }
     }
   }
 
@@ -100,9 +125,16 @@ class Start extends Component {
           <StyledTagLine>
             An open source wallet for your Crypto Collectibles.
           </StyledTagLine>
-          <MetamaskButton onClick={this.checkAccountsLocked}>
+          <ConnectButton
+            color="rgb(246, 133, 27)"
+            onClick={this.checkAccountsLocked}
+          >
             Connect to Metamask
-          </MetamaskButton>
+          </ConnectButton>
+          <br />
+          <ConnectButton color="black" onClick={this.checkLedgerSetup}>
+            Connect to Ledger
+          </ConnectButton>
         </StyledStart>
         <Modal isOpen={accountsLocked} style={customStyles} ariaHideApp={false}>
           <h1>Accounts locked</h1>
--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -1,24 +1,45 @@
 // @format
 import Web3 from "web3";
+import ProviderEngine from "web3-provider-engine";
+import RpcSubprovider from "web3-provider-engine/subproviders/rpc";
+import LedgerWalletSubproviderFactory from "ledger-wallet-provider";
 
-const resolveWeb3 = resolve => {
-  let { web3 } = window;
-  const localProvider = `http://localhost:9545`;
+const resolveWeb3 = async resolve => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const provider = urlParams.get("provider");
 
-  if (window.ethereum) {
-    web3 = new Web3(ethereum);
-    try {
-      window.ethereum.enable().then(() => resolve(web3));
-    } catch (err) {
-      alert(err);
+  if (provider === "metamask" || !provider) {
+    let { web3 } = window;
+    const localProvider = `http://localhost:9545`;
+
+    if (window.ethereum) {
+      web3 = new Web3(ethereum);
+      try {
+        window.ethereum.enable().then(() => resolve(web3));
+      } catch (err) {
+        alert(err);
+      }
+    } else if (window.web3) {
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      const provider = new Web3.providers.HttpProvider(localProvider);
+      web3 = new Web3(provider);
+      resolve(web3);
     }
-  } else if (window.web3) {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    const provider = new Web3.providers.HttpProvider(localProvider);
-    web3 = new Web3(provider);
+  } else if (provider === "ledger") {
+    var engine = new ProviderEngine();
+    var web3 = new Web3(engine);
+
+    var ledgerWalletSubProvider = await LedgerWalletSubproviderFactory();
+    engine.addProvider(ledgerWalletSubProvider);
+    engine.addProvider(
+      new RpcSubprovider({ rpcUrl: "https://mainnet.infura.io" })
+    );
+    engine.start();
+    resolve(web3);
   }
-  resolve(web3);
+
+  resolve(null);
 };
 
 export default () =>
