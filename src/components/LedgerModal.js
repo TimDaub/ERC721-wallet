fe92c279009b3bd226a4b33de48353da8145eda3
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-11-11 21:35:18 +0100

    Add accounts table

--- src/components/LedgerModal.js
+++ src/components/LedgerModal.js
@@ -1,8 +1,49 @@
 // @format
 import React, { Component } from "react";
 import Modal from "react-modal";
+import styled from "styled-components";
+
+import getWeb3 from "../utils/getWeb3";
+
+const List = styled.ul`
+  list-style: none;
+  padding-left: 0;
+  width: 100%;
+  height: 30px;
+`;
+
+const ListElement = styled.li`
+  padding: 5px 0 5px 0;
+  &: nth-child(even) {
+    background-color: #f5f5f5;
+  }
+  &: hover {
+    background-color: #eeeeee;
+    cursor: pointer;
+  }
+`;
+
+const InlineElement = styled.li`
+  font-weight: ${props => (props.bold ? "bold" : "normal")}
+  display: inline-block;
+  float: left;
+  width: ${props => props.width};
+  padding-top: 5px;
+  text-align: center;
+`;
 
 class LedgerModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: []
+    };
+  }
+  async componentDidMount() {
+    const web3 = await getWeb3("ledger");
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ accounts });
+  }
   render() {
     const customStyles = {
       content: {
@@ -17,6 +58,7 @@ class LedgerModal extends Component {
     };
 
     const { isOpen, toggleModal } = this.props;
+    const { accounts } = this.state;
     return (
       <div>
         <Modal
@@ -26,6 +68,37 @@ class LedgerModal extends Component {
           ariaHideApp={false}
         >
           <h1>Select an Address</h1>
+          <List>
+            <ListElement>
+              <List>
+                <InlineElement bold width="10%">
+                  #
+                </InlineElement>
+                <InlineElement bold width="70%">
+                  Address
+                </InlineElement>
+                <InlineElement bold width="20%">
+                  More
+                </InlineElement>
+              </List>
+            </ListElement>
+            {accounts.map((account, i) => (
+              <ListElement key={i}>
+                <List>
+                  <InlineElement width="10%">{i}</InlineElement>
+                  <InlineElement width="70%">{account}</InlineElement>
+                  <InlineElement width="20%">
+                    <a
+                      target="_blank"
+                      href={"https://etherscan.io/address/" + account}
+                    >
+                      more...
+                    </a>
+                  </InlineElement>
+                </List>
+              </ListElement>
+            ))}
+          </List>
         </Modal>
       </div>
     );
